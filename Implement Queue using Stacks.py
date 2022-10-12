a4d0336acb7652b120187e745d0f6bbc71d060d9
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-10-12 20:23:15 +0300

    Create Implement Queue using Stacks.py

---
+++ Implement Queue using Stacks.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.frnt = [] #this stack will represent the front of the queue
+        self.bck = [] #this stack will represent the back of the queue
+
+    def push(self, x: int) -> None:
+        self.bck.append(x) #add to the back
+
+    def pop(self) -> int:
+        self.frontToBack()
+        return self.frnt.pop() #return the top element in the front
+
+    def peek(self) -> int:
+        self.frontToBack()
+        return self.frnt[-1] #return the element at the front of our queue
+
+    def empty(self) -> bool:
+        return len(self.frnt)+len(self.bck)==0 #if both our stacks are empty, the queue is empty
+    
+    def frontToBack(self) -> None:
+        if not self.frnt: #if we don't have anything at the front
+            while self.bck:
+                #add all elements from the back to the front
+                self.frnt.append(self.bck.pop())
