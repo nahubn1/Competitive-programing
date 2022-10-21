dac13edf6ef73239d6fa791e21cc7d922b48918b
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-10-21 23:02:57 +0300

    Update Max Consecutive Ones III.py

--- Max Consecutive Ones III.py
+++ Max Consecutive Ones III.py
@@ -1,38 +1,14 @@
-class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        
-        start  = 0
-        end = 0
-        
-        zeros_pre_sum = [0]
-        count = 0
-        for i in nums:
-            if i == 0:
-                count += 1
-            zeros_pre_sum.append(count)
-        zeros_pre_sum.append(zeros_pre_sum[-1])
-        
-        max_len = 0
-        while start <= end < len(nums):
-            # zeros count the number of zeros that will exist if we expand our window
-            zeros =  zeros_pre_sum[end+1] - zeros_pre_sum[start]
-            # print('s =', start, 'e =', end, 'z =', zeros)
-            if zeros <= k:
-                #expand
-                end+=1
-            else:
-                if start != end:
-                    #contract
-                    start+=1
-                else:
-                    #skip
-                    start+=1
-                    end+=1
+        start = 0
+        zeros_count = 0
+        Max = 0
+        for end in range(len(nums)):
+            if nums[end] == 0:
+                zeros_count += 1
+            while zeros_count > k:
+                if nums[start] == 0:
+                    zeros_count -= 1
+                start += 1
+                
+            Max = max(Max, end - start + 1)
             
-            max_len = max(max_len, end-start)
-            
-        return max_len
-            
-        
-        
-        return length
+        return Ma
