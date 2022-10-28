7d1d0cf9937a60efddcb81f61f55aaaacd1f35e6
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-10-28 13:30:37 +0300

    Update Find Original Array From Doubled Array.py

--- Find Original Array From Doubled Array.py
+++ Find Original Array From Doubled Array.py
@@ -1,38 +1,20 @@
+from collections import Counter
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
         changed.sort()
-        hashmap = {}
-        delete = {}
-        for i in changed:
-            delete[i] = 0
-            if i in hashmap:
-                hashmap[i] += 1
-            else:
-                hashmap[i]  = 1
-                
+        count = Counter(changed)
+        
+        if len(changed)%2 == 1:
+            return []
         
         orginal = []
-        n = len(changed)
-        if n%2 == 1:
-            return []
-        i = 0
-        # print(changed)
-        while i < n-1:
-            # print(i, changed[i])
-            hashmap[changed[i]] -= 1
-            if changed[i]*2 in hashmap and hashmap[changed[i]*2] > 0:
-                hashmap[changed[i]*2] -= 1
-                orginal.append(changed[i])
-                delete[changed[i]*2] += 1
-                # print(delete, changed[i])
-                i += 1
-                while i < n and delete[changed[i]] > 0:
-                    # print('h1', delete, changed[i])
-                    delete[changed[i]] -= 1
-                    i += 1
-                    
-                    
-            else:
-                return []
-            
-        return orginal
+        for num in changed:
+            if count[num]:
+                count[num] -= 1
+                if count[num*2]:
+                    orginal.append(num)
+                    count[num*2] -= 1
+                else:
+                    return []
+        
+        return orginal  
