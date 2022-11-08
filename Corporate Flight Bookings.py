80381128868a515571624b084dbfc6bfac6584fe
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-11-08 16:22:05 +0300

    Update Corporate Flight Bookings.py

--- Corporate Flight Bookings.py
+++ Corporate Flight Bookings.py
@@ -1,12 +1,9 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        res = [0]*(n+1)
-        for start, end, seat in bookings:
-            res[end] += seat
-            res[start-1] += -seat
-        
-        for i in range(len(res)-2, -1, -1):
-            
-            res[i] += res[i+1]
-        res.pop(0)
-        return res
+        flight = [0]*n
+        for s, e, seats, in bookings:
+            flight[s-1] +=  seats
+            if e<n: flight[e] -= seats 
+        for i in range(1, n):
+            flight[i] += flight[i-1]
+        return flight
