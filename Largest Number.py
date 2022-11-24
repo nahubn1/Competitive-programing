583414282466ee1c175845902a2c3afa15838a00
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-11-24 12:03:41 +0300

    Update Largest Number.py

--- Largest Number.py
+++ Largest Number.py
@@ -1,22 +1,11 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         nums.sort(reverse=True)
-        nums = [str(i) for i in nums]
-        for i, num in enumerate(nums):
-            loc = i
-            while loc > 0:
-                if int(num) > int(nums[loc-1][:len(num)]):
-                    loc -= 1
-                elif int(num) == int(nums[loc-1][:len(num)]):
-                    if int(num+nums[loc-1][len(num):]) > int(nums[loc-1][len(num):]+num):
-                        loc -= 1
-                    else:
-                        break
-                else:
-                    break
-            
-            
-            nums.pop(i)
-            nums.insert(loc, num)
-            
-        return str(int(''.join(nums)))
+        nums = [str(num) for num in nums]
+        outPut = []
+        for i in range(len(nums)):
+            pos = i
+            while pos > 0 and int(nums[i]+outPut[pos-1])>int(outPut[pos-1]+nums[i]):
+                pos -= 1
+            outPut.insert(pos, nums[i])
+        return str(int(''.join(outPut)))
